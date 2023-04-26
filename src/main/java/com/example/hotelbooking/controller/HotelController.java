8d584fd7ba21217907a1021f62e20ae0e64feb67
Chathu1011 <shalika1011@gmail.com>
2023-04-26 09:46:23 +0530

    Add bootstrap dependency and create Hotel.html file

--- src/main/java/com/example/hotelbooking/controller/HotelController.java
+++ src/main/java/com/example/hotelbooking/controller/HotelController.java
@@ -1,10 +1,22 @@
 package com.example.hotelbooking.controller;
 
-import com.example.hotelbooking.repository.HotelRepository;
+import com.example.hotelbooking.model.HotelDetails;
+import com.example.hotelbooking.model.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HotelController {
-    @Autowired private HotelRepository repo;
+    @Autowired private HotelService service;
+
+    @GetMapping("/hotels")
+    public String showHotelList(Model model){
+        List<HotelDetails> listHotels = service.listAll();
+        model.addAttribute("listHotels",listHotels);
+        return "hotels";
+    }
 }
--- src/main/java/com/example/hotelbooking/model/HotelService.java
+++ src/main/java/com/example/hotelbooking/model/HotelService.java
@@ -1,4 +1,16 @@
 package com.example.hotelbooking.model;
 
+import com.example.hotelbooking.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class HotelService {
+    @Autowired private HotelRepository repo;
+
+    public List<HotelDetails> listAll(){
+        return (List<HotelDetails>) repo.findAll();
+    }
 }
